05077616ff8ff96807852cdb678585014d0cdfa6
moon9407 <xzc7001@naver.com>
2025-10-23 15:54:03 +0900

    파이썬 수업내용

---
+++ p1023/p06_타입.py
@@ -0,0 +1,17 @@
+# 타입 
+# 숫자(정수,실수)
+# 문자열
+# 불-bool : boolen 참, 거짓
+
+print(100+100)
+#print("100"+100) # 문자열,숫자는 덧셈이 안됨.
+num = "100"       # 문자열
+num2 = 100        # 숫자-정수
+num3 = 100.3        # 숫자-실수
+num4 = True       # bool-참
+num5 = False      # bool-거짓
+# num6 = true     # 에러 (소문자 불가능)
+num7 = "true"     # 타입 - 문자열
+print(type(num7)) # str->, type() 변수의 타입을 확인할 수 있음.
+num8 = None
+# print(num+num2) # 문자열,숫자는 덧셈이 안됨.
