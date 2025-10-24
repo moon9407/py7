2d3af5991713f4e3ea9323dda7e80b56bec82f9d
moon9407 <xzc7001@naver.com>
2025-10-24 14:44:40 +0900

    파이썬 2일차

---
+++ p1024/p10_논리연산자.py
@@ -0,0 +1,16 @@
+# 논리연산자는 if로 사용이 되어짐
+# if(논리연산이 들어감), 0:False를 의미, 그외 숫자는 True
+# input
+# print, print("%s.%d",%f" % (a,b,c))
+# print ("\t,\n,\")
+# +,-,*,/,,//,%,** 산술연산자
+# >,<,==,!=,>=,<= 관계(비교)연산자
+# and.or,not 논리연산자
+# = 대입연산자 ex)num = 10
+if(5>0):
+    print("True")
+    print("참입니다.")
+
+else:
+    print("False")
+    print("거짓입니다.")
