80a4f590b72c66e378add6d7a6b3b12b4b8ebe35
moon9407 <xzc7001@naver.com>
2025-11-03 13:18:08 +0900

    파이썬 8일차

---
+++ P1103/p07_함수매개변수.py
@@ -0,0 +1,17 @@
+def cal(a,b):
+    a+b   # 함수 끝을 만나면 함수 종료됨.
+    # return       # return을 만나면 함수 종료
+
+print(cal(1,2))
+
+# return : 함수를 호출한 곳으로 값을 전달할 때 사용
+def cal(a,b):   #타입 = int  , 매개변수는 타입이 꼭 일치해야함
+    return (a+b)  
+
+num1 = cal(10,5)
+num2 = cal(2,7)
+num3 = cal(3,5)
+
+# 위 세개를 더한 값
+print(num1+num2+num3)
+print(num1-num2-num3)
