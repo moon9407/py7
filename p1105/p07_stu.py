73ad2d03f093cb1a4dffabf0f80d4ddc9032d069
moon9407 <xzc7001@naver.com>
2025-11-05 18:24:04 +0900

    파이썬 10일차

--- p1105/p07_stu.py
+++ p1105/p07_stu.py
@@ -14,7 +14,9 @@ class Student:
 
     # __str__: 객체, 참조변수를 출력하면 함수 실행을 시킴
     def __str__(self):
-        return (f"{self.stuno}\t{self.name}\t{self.kor}\t")
+        return (f"{self.stuno}\t{self.name}\t{self.kor}\t\
+{self.eng}\t{self.math}\t{self.total}\t\
+{self.avg}\t{self.rank}\t")
         
 
     def s_total(self):
@@ -25,15 +27,28 @@ class Student:
 # 학생성적리스트 클래스
 class Students:
     stu_list = []
+    titles = ['번호','이름','국어','영어','수학','합계','평균','등수']
+    def print(self):
+        print(" "*10,"[ 학생성적리스트 ]")
+        print("-"*65)
+        print(*self.titles,sep="\t")
+        print("-"*65)
+        for stus in self.stu_list:
+            print(stus)
+        print()      
+
+    
     def add(self,student):
         self.stu_list.append(student)
 
             
 stus = Students()
             
+# Students 클래스에서 리스트에 추가
 stus.add(Student(10101,'홍길동',100,100,100))
 stus.add(Student(10102,'유관순',90,100,100))
 stus.add(Student(10103,'이순신',80,100,100))
 
 print(Student(10101,'홍길동',100,100,100))
 
+stus.print()
